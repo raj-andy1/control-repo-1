35eb3f0a8641df036a48afaef38c64d2f14c2869
Andy Rajagopalan <ranandthirth@@gmail.com>
2018-09-07 13:59:20 -0700

    remove additional space

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,7 +1,7 @@
 #base profile for demo purpose
 #the base profile should include component modules that will be on all nodes
 
-class profile::base { 
+class profile::base {
   include arapache
 
   package {'tcpdump':
