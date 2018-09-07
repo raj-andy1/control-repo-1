0c46a379b19b02f97c4f3c40787027c22a01537c
Andy Rajagopalan <ranandthirth@@gmail.com>
2018-09-07 11:09:02 -0700

    new profiles

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,11 @@
-class profile::base {
+#base profile for demo purpose
+#the base profile should include component modules that will be on all nodes
 
-  #the base profile should include component modules that will be on all nodes
+class profile::base { 
+  include arapache
+
+  package {'tcpdump':
+  ensure  => latest,
+  }
 
 }
