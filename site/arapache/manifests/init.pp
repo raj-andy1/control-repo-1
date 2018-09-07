98c4aeef53a68d162b0e1fbfea51cc27d8e2f6a0
Andy Rajagopalan <ranandthirth@@gmail.com>
2018-09-07 09:25:21 -0700

    changed files for the demo

---
+++ site/arapache/manifests/init.pp
@@ -0,0 +1,23 @@
+# /etc/puppetlabs/code/environments/production/site/arapache/manifests/init.pp
+# Sample Puppet code to install Apache on a centos machine for demo purposes
+
+class arapache {
+
+  package { 'httpd':
+    ensure  =>  present,
+  }
+
+  service { 'httpd':
+    ensure  =>  running,
+    enable  =>  true,
+    require =>  Package['httpd'],
+  }
+
+  file { '/var/www/html/index.html':
+    ensure  =>  file,
+    mode    =>  '0755',
+    source  =>  'puppet:///modules/arapache/index.html',
+    require =>  Package['httpd'],
+    notify  =>  Service['httpd'],
+  }
+}
